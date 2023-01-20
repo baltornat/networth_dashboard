9d97f27b80b762a91aab26cfe6627b33b032b51a
baltornat <marcocesana98@gmail.com>
2023-01-20 19:36:19 +0100

    Line graph updated

--- networth_dash.py
+++ networth_dash.py
@@ -7,13 +7,29 @@ import plotly.express as px
 
 mynw = pd.read_csv('./csv/mynetworth.csv', sep=',')
 
+categories = [
+
+]
+
+years = [
+    '2023'
+]
+
+### PRELIMINAR OPERATIONS ON THE DATAFRAME
+# Convert the date to datetime64
+mynw['Date'] = pd.to_datetime(mynw['Date'])
+# Fill NaN with 0
+mynw[['Incomings','Expenses']] = mynw[['Incomings','Expenses']].fillna(0)
+# Create daily networth
+mynw['Balance'] = (mynw['Incomings'] - mynw['Expenses']).cumsum()
+
 app = dash.Dash()
 
 app.layout = html.Div(children=[
-    html.H1(children='Net worth Dashboard'),
+    html.H1(children='Net Worth Dashboard'),
     dcc.Dropdown(id='year-dropdown',
         options=[{'label': i, 'value': i}
-            for i in mynw['Year'].unique()]),
+            for i in years]),
     dcc.Graph(id='expenses-graph')
 ])
 
@@ -21,11 +37,9 @@ app.layout = html.Div(children=[
     Output(component_id='expenses-graph', component_property='figure'),
     Input(component_id='year-dropdown', component_property='value')
 )
-def update_graph(selected_year):
-    filtered_networth = mynw[mynw['Year'] == selected_year]
-    line_fig = px.line(filtered_networth,
-        x='Day', y='Expenses',
-        title=f'Expenses in {selected_year}')
+def line_chart(selected_year):
+    filtered_mynw = mynw[mynw['Date'].dt.strftime('%Y') == selected_year]
+    line_fig = px.line(filtered_mynw, x='Date', y='Balance', title=f'Annual Trend ({selected_year})')
     return line_fig
     
 if __name__ == '__main__':
